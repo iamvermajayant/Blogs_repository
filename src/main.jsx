52ca294f050f1c1dce1000fe0e6c9d811d58ab54
Jayant Verma <iamvermajayant@gmail.com>
2023-10-04 20:21:37 +0530

    added material ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#ff5722", // Example primary color
+    },
+    secondary: {
+      main: "#3f51b5", // Example secondary color
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
+);
